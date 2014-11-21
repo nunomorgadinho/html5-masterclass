77a36e3b3cbff9f071d57c6d23875acec6db3e64
Nuno Morgadinho <nuno+rulez@widgilabs.com>
2014-11-21 10:38:06 +0000

    jquery show hide demo

---
+++ jquery-show-hide-demo/js/app.js
@@ -0,0 +1,19 @@
+
+$( document ).ready(function() {
+
+	console.log("hello world");
+
+	$(".logo1").hide("slow", function(){
+		
+	});
+
+	$(".logo2").show("fast", function(){
+		
+	});
+
+	$(".logo3").toggle(1000, function(){
+		
+	});
+
+
+});
