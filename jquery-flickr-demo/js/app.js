0e7192073d49f9c02161b07228e13beba6f791ad
Nuno Morgadinho <nuno+rulez@widgilabs.com>
2014-11-21 11:55:28 +0000

    flickr demo

---
+++ jquery-flickr-demo/js/app.js
@@ -0,0 +1,19 @@
+
+$( document ).ready(function() {
+
+	var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	  $.getJSON( flickerAPI, {
+	    tags: "phc software",
+	    tagmode: "any",
+	    format: "json"
+	  })
+	    .done(function( data ) {
+	      $.each( data.items, function( i, item ) {
+	        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+	        if ( i === 3 ) {
+	          return false;
+	        }
+	      });
+	    });
+
+});
